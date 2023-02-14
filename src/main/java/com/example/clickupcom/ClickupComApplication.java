4900f4e11b1faf7eee8ccdaa7ec8ac903ea13edf
hp <farruxzaitov7176@gmail.com>
2023-02-14 18:49:11 +0500

    commit

---
+++ src/main/java/com/example/clickupcom/ClickupComApplication.java
@@ -0,0 +1,13 @@
+package com.example.clickupcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickupComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickupComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clickupcom/ClickupComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clickupcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickupComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
